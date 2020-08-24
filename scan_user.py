92a0057d67bbc85aeaae160839cf6009ccf85cf7
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2020-08-24 03:58:37 +0200

    Added comments to python files.

--- scan_user.py
+++ scan_user.py
@@ -16,16 +16,18 @@ signed_in = []
 ID = []
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
+
+#  Connects to mongo database.
 api = 'mongodb+srv://User2200:eumeLuzLWXqGxcVd@clock-in-system.6tmxo.mongodb.net/user_clock_In?retryWrites=true&w=majority'
 
 client = pymongo.MongoClient(api)
 db = client.get_database('user_clock_In')
 collection = db.time_table
 
+#  Returns all data from collection time_table.
 data = list(collection.find({}))
 
-#  Create function that update variable
-#  From var use get Id numbers
+#  Returns id from database and update ID list with all id's that exist in the database.
 def update_id():
     try:
         for user in data:
@@ -37,6 +39,7 @@ def update_id():
     return current_id
 
 
+#  If the name doesn't exist it'll be added along with a unique id to the database.
 def check_sign_in(name):
     global current_id
     try:
@@ -54,6 +57,9 @@ def run_scan():
         _, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        
+        #  Draws rectangle around items of interest.
+        #  Adds text to notify the user there are changes in the image/video feed.
         for(x,y,w,h) in faces:
             cv2.rectangle(img, (x,y), (x+w, y+h), (221,160,221), 3)
             
@@ -62,6 +68,8 @@ def run_scan():
             id_, conf = recognizer.predict(roi_gray)
 
             if conf >= 45 and conf <= 85:
+                #  Draws rectangle around items of interest.
+                #  Adds text to notify the user there are changes in the image/video feed.
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 name = labels[id_]
                 color = (30, 255, 255)
@@ -75,16 +83,20 @@ def run_scan():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+    #  Teminates the window displaying the video and end the video streaming process.
     cap.release()
     cv2.destroyAllWindows()
 
-
+#  Get the face cascade file from the root directory of this app.
 face_cascade = cv2.CascadeClassifier(os.path.join(base_dir, r"cascades\haarcascade_frontalface_default.xml"))
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
+#  Recognizer returns trained data that is used to identify familiar faces.
+#  Get the trainner.yml file from the root directory of this app.
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read(os.path.join(base_dir, "trainner.yml"))
 
+#  Get the pickle file from the root directory of this app. 
 with open(os.path.join(base_dir, "labels.pickle"), "rb") as data_file:
     labels = pickle.load(data_file)
     labels = {v:k for k,v in labels.items()}
@@ -92,7 +104,7 @@ with open(os.path.join(base_dir, "labels.pickle"), "rb") as data_file:
 #  Set cap parameters to 0 to enable a webcam. Current revieves feed from a ip webcam.
 cap = cv2.VideoCapture(r"http://192.168.43.1:8080/video")
 
-
+#  Runs if file is ran locally
 if __name__ == "__main__":
     update_id()
     run_scan()
--- train.py
+++ train.py
@@ -8,8 +8,11 @@ from PIL import Image
 #  Gets all the images in the folder train.
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
+#  Navigates to the directory train within the root directory.
 img_dir = os.path.join(base_dir, "train")
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+#  Get the face cascade file from the root directory of this app.
 face_cascade = cv2.CascadeClassifier(os.path.join(base_dir, r"cascades\haarcascade_frontalface_default.xml"))
 
 current_id = 0
@@ -19,6 +22,7 @@ x_train = []
 
 for root, dirs, files in os.walk(img_dir):
     for file in files:
+        #  Finds all files ending with .png and .jpg. It then return their full path along with their names.
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
             label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
@@ -27,22 +31,27 @@ for root, dirs, files in os.walk(img_dir):
                 label_ids[label] = current_id
                 current_id += 1
             id_ = label_ids[label]
-
+            
+            #  Resizes images for more effective machine learning results.
             pil_img = Image.open(path).convert("L")
             size = (550, 550)
             final_img = pil_img.resize(size, Image.ANTIALIAS)
+            
+            #  Convert image to a numpy array.
             img_array = np.array(final_img, "uint8")
             
             faces = face_cascade.detectMultiScale(img_array, 1.3, 5)
-
+            
             for (x, y, w, h) in faces:
                 roi = img_array[y:y+h, x:x+w]
                 x_train.append(roi)
                 y_label.append(id_)
 
+#  Creates a pickle file that contains binary code.
 with open(os.path.join(base_dir, "labels.pickle"), "wb") as data_file:
     pickle.dump(label_ids, data_file)
 
+#  Uses open cv recognizer and passes trained values to it and save as a .yml file.
 recognizer.train(x_train, np.array(y_label))
 recognizer.save(os.path.join(base_dir, "trainner.yml"))
 
