35b456b5a0592ac8c75b5b3e659b76499225e8c5
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2020-08-24 02:40:02 +0200

    Uploaded motion_detector.py

---
+++ motion_detector.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(r'http://192.168.43.1:8080/video')
+
+ret, frame1 = cap.read()
+ret, frame2 = cap.read()
+
+while cap.isOpened():
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=1)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        
+        if cv2.contourArea(contour) < 700:
+            continue
+        cv2.rectangle(frame1, (x,y), (x+w, y+h), (255,0,0), 2)
+        cv2.putText(frame1, "[*] Status: {}".format('Motion Detected'), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 100, 0),2)
+        
+    #cv2.drawContours(frame1, contours, -1, (255 ,255, 130), 1)
+
+    cv2.imshow("feed", frame1)
+    frame1 = frame2
+    ret, frame2 = cap.read()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
